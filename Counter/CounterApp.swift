a219d75a079ef36a29d80c76604144e1da71c1b0
kota-aso <kotaaso1212@gmail.com>
2024-04-02 21:11:57 +0900

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Kota Aso on 2024/04/02.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
